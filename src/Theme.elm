67df2b3be3d193eca404769cd0bc9b422b48b99c
Sindre Teigland <sindre.teigland@rainfall.no>
2018-12-02 20:40:59 +0100

    Added calculations for text color so it can set best contrast

--- src/Theme.elm
+++ src/Theme.elm
@@ -1,7 +1,8 @@
-module Theme exposing (bobafett, customCssProperties, defaultTheme, chill, rgbToContrastColor)
+module Theme exposing (RGB, bobafett, chill, customCssProperties, defaultTheme, fromString, fromStringHelp, hexToRgb, intToRgb, rgbToContrastColor)
 
 import Html exposing (..)
 import Html.Attributes exposing (attribute)
+import Regex
 
 
 defaultTheme =
@@ -21,11 +22,11 @@ defaultTheme =
 
 bobafett =
     [ ( "primary-color", "#71262D" )
-    , ( "primary-text-color", "#fff" )
+    , ( "primary-text-color", createContextColor "71262D" )
     , ( "accent-color", "#F3C72A" )
     , ( "accent-text-color", "#000" )
     , ( "theme-color", "#A19AA1" )
-    , ( "theme-text-color", "#fff" )
+    , ( "theme-text-color", createContextColor "#A19AA1" )
     , ( "theme-appbar", "#5E6E63" )
     , ( "theme-appbar-text", "#fff" )
     , ( "theme-input-bar", "#5E6E63" )
@@ -47,6 +48,17 @@ chill =
     , ( "theme-input-field", "#303030" )
     , ( "theme-input-field-text", "#fff" )
     ]
+
+
+type alias RGB =
+    { red : Int
+    , green : Int
+    , blue : Int
+    }
+
+
+createContextColor hexString = 
+    hexToRgb hexString |> rgbToContrastColor 
 
 
 rgbToContrastColor rgbColor =
@@ -74,3 +86,150 @@ customCssProperties styles =
                     ""
     in
     attribute "style" css
+
+
+
+--This is not that robust, but its a start
+
+
+hexToRgb hexString =
+    let
+        intValue =
+            String.replace "#" "" hexString |> fromString
+        
+    in
+        intToRgb intValue
+
+
+intToRgb : Result String Int -> RGB
+intToRgb integer =
+    case integer of
+        Ok number ->
+            let
+                blue =
+                    modBy 256 number
+
+                red =
+                    modBy 256 <| floor (toFloat number / 256 / 256)
+
+                green =
+                    modBy 256 <| floor (toFloat number / 256)
+            in
+            { red = red, green = green, blue = blue }
+
+        Err err ->
+            { red = 0, green = 0, blue = 0 }
+
+
+fromString : String -> Result String Int
+fromString str =
+    if String.isEmpty str then
+        Err "Empty strings are not valid hexadecimal strings."
+
+    else
+        let
+            result =
+                if String.startsWith "-" str then
+                    let
+                        list =
+                            str
+                                |> String.toList
+                                |> List.tail
+                                |> Maybe.withDefault []
+                    in
+                    fromStringHelp (List.length list - 1) list 0
+                        |> Result.map negate
+
+                else
+                    fromStringHelp (String.length str - 1) (String.toList str) 0
+
+            formatError err =
+                String.join " "
+                    [ "\"" ++ str ++ "\""
+                    , "is not a valid hexadecimal string because"
+                    , err
+                    ]
+        in
+        Result.mapError formatError result
+
+
+fromStringHelp : Int -> List Char -> Int -> Result String Int
+fromStringHelp position chars accumulated =
+    case chars of
+        [] ->
+            Ok accumulated
+
+        char :: rest ->
+            -- NOTE: It's important to have this call `fromStringHelp` directly.
+            -- Previously this called a helper function, but that meant this
+            -- was not tail-call optimized; it did not compile to a `while` loop
+            -- the way it does now. See 240c3d5aa4f97463b924728935d2989621e9fd6b
+            case char of
+                '0' ->
+                    fromStringHelp (position - 1) rest accumulated
+
+                '1' ->
+                    fromStringHelp (position - 1) rest (accumulated + (16 ^ position))
+
+                '2' ->
+                    fromStringHelp (position - 1) rest (accumulated + (2 * (16 ^ position)))
+
+                '3' ->
+                    fromStringHelp (position - 1) rest (accumulated + (3 * (16 ^ position)))
+
+                '4' ->
+                    fromStringHelp (position - 1) rest (accumulated + (4 * (16 ^ position)))
+
+                '5' ->
+                    fromStringHelp (position - 1) rest (accumulated + (5 * (16 ^ position)))
+
+                '6' ->
+                    fromStringHelp (position - 1) rest (accumulated + (6 * (16 ^ position)))
+
+                '7' ->
+                    fromStringHelp (position - 1) rest (accumulated + (7 * (16 ^ position)))
+
+                '8' ->
+                    fromStringHelp (position - 1) rest (accumulated + (8 * (16 ^ position)))
+
+                '9' ->
+                    fromStringHelp (position - 1) rest (accumulated + (9 * (16 ^ position)))
+
+                'a' ->
+                    fromStringHelp (position - 1) rest (accumulated + (10 * (16 ^ position)))
+
+                'b' ->
+                    fromStringHelp (position - 1) rest (accumulated + (11 * (16 ^ position)))
+
+                'c' ->
+                    fromStringHelp (position - 1) rest (accumulated + (12 * (16 ^ position)))
+
+                'd' ->
+                    fromStringHelp (position - 1) rest (accumulated + (13 * (16 ^ position)))
+
+                'e' ->
+                    fromStringHelp (position - 1) rest (accumulated + (14 * (16 ^ position)))
+
+                'f' ->
+                    fromStringHelp (position - 1) rest (accumulated + (15 * (16 ^ position)))
+
+                'A' ->
+                    fromStringHelp (position - 1) rest (accumulated + (10 * (16 ^ position)))
+
+                'B' ->
+                    fromStringHelp (position - 1) rest (accumulated + (11 * (16 ^ position)))
+
+                'C' ->
+                    fromStringHelp (position - 1) rest (accumulated + (12 * (16 ^ position)))
+
+                'D' ->
+                    fromStringHelp (position - 1) rest (accumulated + (13 * (16 ^ position)))
+
+                'E' ->
+                    fromStringHelp (position - 1) rest (accumulated + (14 * (16 ^ position)))
+
+                'F' ->
+                    fromStringHelp (position - 1) rest (accumulated + (15 * (16 ^ position)))
+
+                nonHex ->
+                    Err (String.fromChar nonHex ++ " is not a valid hexadecimal character.")
